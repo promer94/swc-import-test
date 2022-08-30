14b8fa877b450714b68505bc39d18e5065e55e17
ekkoxu <ekkoxu@tencent.com>
2022-08-30 21:06:05 +0800

    import test

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,6 +1,8 @@
 import Vue from "vue"
 import App from "./App.vue"
 import VCA from "@vue/composition-api"
+import ElementUI from "element-ui"
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 Vue.use(VCA)
 new Vue({
